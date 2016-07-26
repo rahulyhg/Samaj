9251703291f5ca3366e84044dd7d98f8410ba22e
Harsh Kumar <HarshVardhanKumar@users.noreply.github.com>
2016-07-26 20:13:53 +0530

    remote interface of isuueF

---
+++ src/java/Userisuue/remotisuue.java
@@ -0,0 +1,11 @@
+package Userisuee;
+
+import java.util.List;
+
+public interface remotisuue {
+    public void createIsuue(String name , String details , String user);
+    public List<isuue> getAllisuue();
+    public String getUser(String nameisuue) ;
+    public String getDetails(String nameisuue) ;
+    public List<isuue> getALLWuser(String userr) ;
+}
