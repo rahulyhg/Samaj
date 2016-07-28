1ab2a1302e5664dd9193438643ceb10616988bb9
Harsh Kumar <HarshVardhanKumar@users.noreply.github.com>
2016-07-28 22:30:01 +0530

    Update and rename src/java/USERD/UserWork.java to src/main/java/USERD/UserWork.java

--- src/main/java/USERD/UserWork.java
+++ src/main/java/USERD/UserWork.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package USERD;
 
 import java.util.List;
 
-/**
- *
- * @author DELL
- */
 public interface UserWork {
     public void createPost(String head , String paragraph , String Userr , String emaill , String isuue);
     public List getPostWUser(String emaill) ;
