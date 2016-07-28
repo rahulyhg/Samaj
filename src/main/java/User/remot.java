7af8b0b40747d2260e8a469530528c79b3a04095
Harsh Kumar <HarshVardhanKumar@users.noreply.github.com>
2016-07-28 22:33:38 +0530

    Update and rename src/java/User/remot.java to src/main/java/User/remot.java

--- src/main/java/User/remot.java
+++ src/main/java/User/remot.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package User;
 
 import java.util.List;
 
-/**
- *
- * @author DELL
- */
 public interface remot {
     public void createNewUser(String Name , String emaill , String Password);
     public USERRinn getUser(String emaill) ;
